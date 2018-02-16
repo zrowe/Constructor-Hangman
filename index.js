a570c560a76a7d2a9b37f0efe6084d110ce9622c
zrowe <paul.louis.rowe@gmailcom>
2018-02-15 23:05:53 -0800

    initialzig files

---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
