a6ce3bc6fe4a4f9bc3205896d3c2e73b723a1cb4
zrowe <paul.louis.rowe@gmailcom>
2018-02-21 00:55:05 -0800

    adding color

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var prompt = require('prompt');
 var Word = require("./word.js");
+var colors = require('colors');
+var banner = require('simple-banner');
 
-// Randomly selects a word and uses the Word constructor to store it
+banner.set("The Amazing Constructor-Hangman", "Cobbled together by zrowe", 0);
 
+
+function GuessTracker() {
+    this.allowed = 10;
+    this.remaining = this.allowed;
+    this.missList = "";
+    this.update = function(char) {
+        if (!this.missList.includes(char)) { // check if character is in miss list. 
+            this.missList = this.missList + char; // add char to the miss list
+            this.remaining--; //decrement remaining
+        }
+    }
+}
 
 prompt.start();
 prompt.message = "";
@@ -21,38 +35,43 @@ var schema = {
     }
 };
 
-
-var count = 3;
-
 function getGuess() {
 
-    if (count > 0) {
+    if (myGuesses.remaining > 0) {
+
         prompt.get(schema, function(err, result) {
-            if (secretWord.guess(result.letter.toLowerCase())) {
-                console.log("Correct");
+            if (err) { console.log(err); process.exit() };
+            var char = result.letter.toLowerCase()
+            if (mySecretWord.guess(char)) {
+                console.log("Correct".green);
             } else {
-                console.log("Incorrect");
+                console.log("Incorrect".red);
+                myGuesses.update(char);
+                console.log("You have chosen these letters badly: " + myGuesses.missList);
+                console.log("You have " + myGuesses.remaining + " guesses remaining");
             }
-            console.log(secretWord.getDisplay());
-            count--;
-            getGuess();
+            console.log(mySecretWord.getDisplay());
+            if (mySecretWord.getDisplay().includes("_")) {
+                getGuess();
+            } else {
+                console.log("You won!".rainbow);
+            };
+
         });
-    }
+    };
 }
 
 
 // Initialize
 var wins = 0;
 var loses = 0;
+var myGuesses = new GuessTracker();
+// Randomly selects a word and uses the Word constructor to store it
+var mySecretWord = new Word("xyzzy"); // store in constructor
 
-var guesses = {
-    allowed: 10,
-    remaining: 0,
-    missList: ""
-}
 
-var count = 3;
 
-var secretWord = new Word("xyzzy"); // store in constructor
+console
+
 
 getGuess();
--- word.js
+++ word.js
@@ -12,11 +12,9 @@ var Word = function(secretWord) {
         this.word.push(new Letter(secretWord[i]));
     };
 
-    // A function that returns a string representing the word. This should call the function 
-    //     on each letter object (the first function defined in Letter.js) that displays the 
-    //     character or an underscore and concatenate those together.
+    // A function that returns a string representing the word known to date.
     this.getDisplay = function() {
-        var str = this.word.join(" ");
+        var str = this.word.join(" "); // put a space between each letter of the word
         return str
     };
     // A function that takes a character as an argument and calls the guess function on each 
@@ -26,7 +24,7 @@ var Word = function(secretWord) {
         for (i = 0; i < this.word.length; i++) {
             if (this.word[i].compare(character)) {result = true;};
         }
-        return result
+        return result // return true if at least one match was found 
     };
 }
 module.exports = Word;
